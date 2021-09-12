6a9f52cd3d57ffdc886921500d6871cc00293021
ivan-v <navioriv@gmail.com>
2021-09-12 16:56:56 -0400

    Initial commit of characters class + unit test

---
+++ src/characters.py
@@ -0,0 +1,6 @@
+class Characters:
+
+    def __init__(self, appearances):
+        self.appearances = appearances
+
+
---
+++ src/test/test_characters.py
@@ -0,0 +1,6 @@
+import characters
+
+def test_characters():
+    characters_obj = characters.Characters({'a': [(0, 10), (13, 20)], 'b': [(4, 6), (15, 25)]}) 
+    assert characters_obj.appearances == {'a': [(0, 10), (13, 20)], 'b': [(4, 6), (15, 25)]}
+
