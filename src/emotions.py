469e895e57f95c4bca128188f6df08de1c2fe03d
ivan-v <navioriv@gmail.com>
2021-09-11 20:59:12 -0400

    Add emotions & unit test

---
+++ src/emotions.py
@@ -0,0 +1,44 @@
+
+def find_point(current_time, absolute_times, values):
+    closest_times = find_nearest_values(current_time, absolute_times)
+    if ('smaller_val' not in closest_times and 'larger_val' in closest_times) or \
+       ('larger_val' not in closest_times and 'smaller_val' in closest_times):
+        location = 'larger_val' if 'larger_val' in closest_times else 'smaller_val'
+        return values[closest_times[location][0]]
+    else:
+        coord1 = (closest_times['smaller_val'][1], values[closest_times['smaller_val'][0]])
+        coord2 = (closest_times['larger_val'][1], values[closest_times['larger_val'][0]])
+        return find_y_at_x(coord1, coord2, current_time)
+
+
+def find_y_at_x(coord1, coord2, x3):
+    return (((coord2[1] - coord1[1]) / (coord2[0] - coord1[0]))*(x3 - coord1[0])) + coord1[1]
+
+
+def find_nearest_values(value, arr):
+    # assumes sorted list 
+    prev_val = ''
+    smaller_val = ''
+    for i, val in enumerate(arr):
+        if val < value:
+            smaller_val = val
+            smaller_i = i
+        elif val > value:
+            if smaller_val != '':
+                return {'smaller_val': (smaller_i, smaller_val), 'larger_val': (i, val)}
+            else:
+                return {'larger_val': (i, val)}
+        prev_val = val
+    return {'smaller_val': (len(arr)-1, prev_val)}
+
+
+def emotions_by_absolute_times(emotions, absolute_times):
+    return list(zip(*[[emotions[emotion][absolute_times.index(current_time)] if current_time in absolute_times 
+           else find_point(current_time, absolute_times, emotions[emotion]) for current_time in range(absolute_times[-1]+1)]
+        for emotion in emotions]))
+
+
+class Emotions:
+    def __init__(self, given_emotions, absolute_times):
+        self.emotions = emotions_by_absolute_times(given_emotions, absolute_times)
+
