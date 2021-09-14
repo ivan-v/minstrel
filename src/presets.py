45cd5715d72c38fa623e4b5d561c4e8f5f02f1ac
ivan-v <navioriv@gmail.com>
2021-09-13 21:53:21 -0400

    Add chapters to presets (placeholder)

--- src/presets.py
+++ src/presets.py
@@ -4,7 +4,15 @@ import emotions
 
 class Presets:
 
-    def __init__(self, emotions_obj, characters_obj, **kwargs):
+    def __init__(self, emotions_obj, characters_obj, time, **kwargs):
         self.seed = kwargs["seed"] if "seed" in kwargs else randint(10000000, 100000000)
         seed(self.seed)
 
+
+        self.time = time
+        self.emotions = emotions_obj
+        self.characters = characters_obj
+
+        # TODO: infer chapter-chunks from ambiance + emotions
+        self.chapters = [(i, i+100) for i in range(0, self.time, 100)]
+        # temp /\
--- src/test/test_presets.py
+++ src/test/test_presets.py
@@ -6,8 +6,12 @@ from test_emotions import example_emotions_obj
 from test_characters import example_characters_obj
 
 def test_presets_seed():
-	presets_obj_1 = presets.Presets(example_emotions_obj, example_characters_obj)
+	example_time = 300 # 300 seconds
+	presets_obj_1 = presets.Presets(example_emotions_obj, example_characters_obj, 300)
 	assert presets_obj_1.seed is not None
 
-	presets_obj_2 = presets.Presets(example_emotions_obj, example_characters_obj, seed=12345678)
+	presets_obj_2 = presets.Presets(example_emotions_obj, example_characters_obj, 300, seed=12345678)
 	assert presets_obj_2.seed == 12345678	
+
+
+example_presets_obj = presets.Presets(example_emotions_obj, example_characters_obj, 300)
